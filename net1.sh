58e76c25c853e62e07b550e3629d4854a1087eff
nidkit1234forex <nidkit1234forex@gmail.com>
2024-11-04 08:26:24 +0700

    Update net1.sh

--- net1.sh
+++ net1.sh
@@ -3,7 +3,7 @@
 site='www.google.com'
 until $(ping -q -c1 ${site} > /dev/null 2>&1)
 do
-    echo "${site} is unreachable. Retrying"
+    echo "${site} is unreachable. Retrying(1)"
 sleep  15s
 ./net2.sh
     # continue
